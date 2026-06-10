ae65c18b8e637fe141622e662e6a6e710fc521ec
Перевалов Кирилл <l1rikalimp@MacBook-Air-Perevalov.local>
2026-06-10 10:07:14 +0300

    after thirth lection, configure base.html and includes templates. adding styles to form widgets

--- clothes_shop/catalogue/forms.py
+++ clothes_shop/catalogue/forms.py
@@ -14,4 +14,15 @@ class ClotheForm(forms.ModelForm):
         'category', 
         'collection', 
         'is_exists'
-    ]
+        ]
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'style': 'border: 2px solid pink'}), 
+            'description': forms.Textarea(attrs={'class': 'form-control'}), 
+            'photo': forms.FileInput(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}), 
+            'color': forms.TextInput(attrs={'class': 'form-control'}), 
+            'size': forms.NumberInput(attrs={'class': 'form-control'}), 
+            'category': forms.Select(attrs={'class': 'form-control'}), 
+            'collection': forms.SelectMultiple(attrs={'class': 'form-control'}), 
+            'is_exists': forms.CheckboxInput(attrs={'class': 'form-control'}),
+        }
--- clothes_shop/clothes_shop/settings.py
+++ clothes_shop/clothes_shop/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'clothes_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
